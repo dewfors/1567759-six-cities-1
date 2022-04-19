135a34d0995da951a99765552c9f83278591ef1f
Zaur Malakhov <1zaur1@mail.ru>
2022-04-19 14:12:59 +0300

    1-2-17 Добавит класс для чтения tsv-файлов.

---
+++ src/common/file-reader/tsv-file-reader.ts
@@ -0,0 +1,32 @@
+import { readFileSync } from 'fs';
+import { Offer } from '../../types/offer.type.js';
+import { FileReaderInterface } from './file-reader.interface.js';
+
+export default class TSVFileReader implements FileReaderInterface {
+  private rawData = '';
+
+  constructor(public filename: string) { }
+
+  public read(): void {
+    this.rawData = readFileSync(this.filename, { encoding: 'utf8' });
+  }
+
+  public toArray(): Offer[] {
+    if (!this.rawData) {
+      return [];
+    }
+
+    return this.rawData
+      .split('\n')
+      .filter((row) => row.trim() !== '')
+      .map((line) => line.split('\t'))
+      .map(([title, description, createdDate, cityName, price, housingType]) => ({
+        title,
+        description,
+        postDate: new Date(createdDate),
+        city: cityName,
+        type: housingType,
+        price: Number.parseInt(price, 10),
+      }));
+  }
+}
--- src/types/offer.type.ts
+++ src/types/offer.type.ts
@@ -1,24 +1,9 @@
-import {City} from './city.type.js';
-import { GoodsType } from './goods.type.js';
-import {HousingType} from './housing.type.js';
-import { Location } from './location.type.js';
-import {User} from './user.type.js';
 
 export type Offer = {
   title: string;
   description: string;
   postDate: Date;
-  city: City;
-  previewImage: string;
-  images: string[];
-  isPremium: boolean;
-  rating: number;
-  type: HousingType;
-  rooms: number;
-  maxAdults: number;
+  city: string;
+  type: string;
   price: number;
-  goods: GoodsType[];
-  author: User;
-  countComments: number;
-  location: Location;
 };
