ed1fd9153d3bbea333276f416dc0e235e32bb5d8
Zaur Malakhov <1zaur1@mail.ru>
2022-04-25 13:22:03 +0300

    2-4-5 Добавит генератор объявлений.

--- src/types/city.type.ts
+++ src/types/city.type.ts
@@ -1,7 +1,7 @@
 import {Location} from './location.type.js';
 import {cityNamesType} from './city-names.type.js';
 
-export type City = {
+export type CityType = {
   name: cityNamesType;
   location: Location;
 };
--- src/types/mock-data.type.ts
+++ src/types/mock-data.type.ts
@@ -1,9 +1,12 @@
+import {CityType} from './city.type.js';
+
 export type MockData = {
   users: string[];
   emails: string[];
   titles: string[];
   descriptions: string[];
-  cities: string[];
+  cities: CityType[];
   types: string[];
   goods: string[];
+  offerImages: string[];
 };
