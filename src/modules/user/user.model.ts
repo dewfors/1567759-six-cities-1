4c1ba1ad0d0177ce10c5046895df2c0e0600d5fa
Zaur Malakhov <1zaur1@mail.ru>
2022-05-10 10:29:37 +0300

    4-1-10 Расширит интерфейс UserDocument. WIP

--- src/modules/user/user.model.ts
+++ src/modules/user/user.model.ts
@@ -1,13 +1,14 @@
 import mongoose from 'mongoose';
 import {User} from '../../types/user.type.js';
+import {UserType} from '../../types/user-type.enum.js';
 
 export interface UserDocument extends User, mongoose.Document {}
 
 const userSchema = new mongoose.Schema({
+  name: String,
   email: String,
   avatarPath: String,
-  firstname: String,
-  lastname: String,
+  type: UserType,
 });
 
 export const UserModel = mongoose.model<UserDocument>('User', userSchema);
