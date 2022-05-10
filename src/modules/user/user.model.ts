29a20cc04158c30d6d0d808ef5bf19a5960d1d5e
Zaur Malakhov <1zaur1@mail.ru>
2022-05-10 10:38:13 +0300

    4-1-11 Добавит в модель timestamps

--- src/modules/user/user.model.ts
+++ src/modules/user/user.model.ts
@@ -2,13 +2,18 @@ import mongoose from 'mongoose';
 import {User} from '../../types/user.type.js';
 import {UserType} from '../../types/user-type.enum.js';
 
-export interface UserDocument extends User, mongoose.Document {}
+export interface UserDocument extends User, mongoose.Document {
+  createdAt: Date,
+  updatedAt: Date,
+}
 
 const userSchema = new mongoose.Schema({
   name: String,
   email: String,
   avatarPath: String,
   type: UserType,
+}, {
+  timestamps: true,
 });
 
 export const UserModel = mongoose.model<UserDocument>('User', userSchema);
