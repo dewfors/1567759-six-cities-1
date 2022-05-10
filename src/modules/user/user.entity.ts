0ed5a9af4f1b0789233aae56be229e992f6d1267
Zaur Malakhov <1zaur1@mail.ru>
2022-05-10 15:15:39 +0300

    4-1-15 Добавит описание схемы. WIP

--- src/modules/user/user.entity.ts
+++ src/modules/user/user.entity.ts
@@ -1,9 +1,21 @@
+import typegoose, {getModelForClass} from '@typegoose/typegoose';
 import {UserType} from '../../types/user-type.enum.js';
 import {User} from '../../types/user.type.js';
 
+const {prop} = typegoose;
+
 export class UserEntity implements User {
+  @prop()
   public name!: string;
+
+  @prop({ unique: true, required: true })
   public email!: string;
+
+  @prop()
   public avatarPath!: string;
+
+  @prop()
   public type!: UserType;
 }
+
+export const UserModel = getModelForClass(UserEntity);
