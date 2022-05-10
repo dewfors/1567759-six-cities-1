99f6548600acdd06a943e89150a902e45d1aa41c
Zaur Malakhov <1zaur1@mail.ru>
2022-05-10 16:06:28 +0300

    4-1-19 Добавит методы в модель UserModel.

--- src/modules/user/user.entity.ts
+++ src/modules/user/user.entity.ts
@@ -2,26 +2,50 @@ import typegoose, {getModelForClass} from '@typegoose/typegoose';
 import {Base, TimeStamps} from '@typegoose/typegoose/lib/defaultClasses.js';
 import {UserType} from '../../types/user-type.enum.js';
 import {User} from '../../types/user.type.js';
+import {createSHA256} from '../../utils/common.js';
 
-const {prop} = typegoose;
+const {prop, modelOptions} = typegoose;
 
 export interface UserEntity extends Base {}
 
+@modelOptions({
+  schemaOptions: {
+    collection: 'users'
+  }
+})
+
 export class UserEntity extends TimeStamps implements User {
-  @prop()
+  constructor(data: User) {
+    super();
+
+    this.name = data.name;
+    this.email = data.email;
+    this.avatarPath = data.avatarPath;
+    this.type = data.type;
+  }
+
+  @prop({required: true, default: ''})
   public name!: string;
 
   @prop({ unique: true, required: true })
   public email!: string;
 
-  @prop()
+  @prop({required: true, default: ''})
   public avatarPath!: string;
 
-  @prop()
+  @prop({required: true, default: 'regular'})
   public type!: UserType;
 
-  @prop()
-  public password!: string;
+  @prop({required: true, default: ''})
+  private password!: string;
+
+  public setPassword(password: string, salt: string) {
+    this.password = createSHA256(password, salt);
+  }
+
+  public getPassword() {
+    return this.password;
+  }
 }
 
 export const UserModel = getModelForClass(UserEntity);
