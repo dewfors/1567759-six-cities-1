2881b6d0c07dd7092761c75cf72051e9a7cc1fe8
Zaur Malakhov <1zaur1@mail.ru>
2022-05-10 15:34:02 +0300

    4-1-17 Добавит поля createdAt, updatedAt.

--- src/modules/user/user.entity.ts
+++ src/modules/user/user.entity.ts
@@ -1,10 +1,11 @@
 import typegoose, {getModelForClass} from '@typegoose/typegoose';
+import {TimeStamps} from '@typegoose/typegoose/lib/defaultClasses.js';
 import {UserType} from '../../types/user-type.enum.js';
 import {User} from '../../types/user.type.js';
 
 const {prop} = typegoose;
 
-export class UserEntity implements User {
+export class UserEntity extends TimeStamps implements User {
   @prop()
   public name!: string;
 
